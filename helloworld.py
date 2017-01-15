f47e4152ae278b82e2b770198c4355f2fdfcfdde
GeekXuan <1316890970@qq.com>
2017-01-15 20:34:11 +0800

    modify helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-from flask iport Flask
-app = Flask[__name__]
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'alert("nice to meet you!")'
+    return '<h3>nice to meet you!</h3>'
 
 if __name__=='__main__':
     app.run(debug = True)
